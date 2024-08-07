6ea8733db0b838decd046b76aeffe26f2672917b
Pepwt <pa632004@gmail.com>
2024-08-07 13:07:45 -0300

    Add views na Activity

--- app/src/main/java/com/pepwt/orgs/MainActivity.kt
+++ app/src/main/java/com/pepwt/orgs/MainActivity.kt
@@ -2,12 +2,16 @@ package com.pepwt.orgs
 
 import android.app.Activity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
 import android.widget.Toast
 
 class MainActivity : Activity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        Toast.makeText(this, "Welcome to Orgs", Toast.LENGTH_SHORT).show()
+       val view = TextView(this)
+        view.setText("Cesta de frutas")
+       setContentView(view)
     }
 }
