2dc8ae36fbb634794e41723009d5d935fb5f2d2a
Pepwt <pa632004@gmail.com>
2024-08-07 12:59:36 -0300

    Executando o App no dispositivo móvel

--- app/src/main/java/com/pepwt/orgs/MainActivity.kt
+++ app/src/main/java/com/pepwt/orgs/MainActivity.kt
@@ -1,7 +1,13 @@
 package com.pepwt.orgs
 
 import android.app.Activity
+import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : Activity() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Toast.makeText(this, "Welcome to Orgs", Toast.LENGTH_SHORT).show()
+    }
 }
